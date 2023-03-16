7ca87af7397a51b056b1bec7ce0b084a6148c08e
boruto <kudruashovanasty97@mail.ru>
2023-03-16 14:31:15 +0300

    first test

--- config/__init__.py
+++ config/__init__.py
@@ -1,8 +1,10 @@
 from config.url import Url
 from config.expectations import Expectations
 from config.playwright import Playwright
+from pages.index_page import IndexPage
 
 
 url = Url()
 playwright = Playwright()
 expectations = Expectations()
+index_page = IndexPage()
